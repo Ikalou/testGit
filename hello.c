ac604b011f6426fe07e371796b5dfff94dce5fbd
Vianney PETIT <vpetit@3dduo.com>
2016-11-22 10:26:56 +0100

    Say hello to someone
    Fix a bug with no args
    
    Tweak return value

--- hello.c
+++ hello.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-	printf("Hello World!\n");
+int main(int argc, char** argv) {
+	if (argc < 2) {
+		printf("Usage: ./a.out <name>\n");
+		exit(0);
+	}
+	printf("Hello %s!\n", argv[1]);
+	return (0);
 }
